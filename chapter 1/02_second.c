4ac63294da0bc1727bc088a5041d3c327708010a
Himanshu Singh <hmsingh7227@gmail.com>
2026-07-31 08:54:13 +0530

    Added Chapter 1 - variables, constants, keywords

---
+++ chapter 1/02_second.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 8;
+     // variable declaration
+    printf("%d",a);
+    return 0;
+}
+
+
---
+++ chapter 1/practice_set.c
@@ -0,0 +1,12 @@
+// Program to calculate the area of a rectangle 
+#include <stdio.h>
+
+int main() {
+    int breadth = 6;
+    int length = 10;
+    printf("area of rectangle is %d", breadth * length);
+    return 0; }
+
+// Output: area of rectangle is 60
+
+ 
