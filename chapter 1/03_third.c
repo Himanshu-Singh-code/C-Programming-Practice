c9f7d26443f7ecdd4e68a30fdc00e34d0d3548f1
Himanshu Singh <hmsingh7227@gmail.com>
2026-08-09 12:51:21 +0530

    Add files via upload

---
+++ chapter 1/03_third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    float b = 2.5;
+    char c = 'A';
+
+
+    printf("%d %f %c", a, b, c);
+    return 0;
+}
---
+++ chapter 1/05_escape_sequence.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// for a new line we use \n escape sequence
+
+int main() {
+    printf("my name is \n Himanshu\n ");
+
+// for some gap we use \t escape sequence  
+printf("Himanshu \t Singh");  
+
+    return 0;
+
+}
+
