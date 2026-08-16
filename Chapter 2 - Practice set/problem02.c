4a4ea8aac134d87744b512249acc32093d6d1bc2
Himanshu Singh <hmsingh7227@gmail.com>
2026-08-16 13:04:34 +0530

    Added : chapter 2 - practice set

---
+++ Chapter 2 - Practice set/problem02.c
@@ -0,0 +1,10 @@
+// Checking whether the no 234235 is divisible by 96 :
+
+
+
+#include <stdio.h>
+  int main() {
+int a = 234235;
+printf("%d\n", a%96); 
+   return 0;
+ }
---
+++ Chapter 2 - Practice set/problem03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+    printf("%d\n", 3*x/y- z+k);
+   // left to right associativity is used in this expression, so the expression is evaluated as follows:
+    return 0;
+}
